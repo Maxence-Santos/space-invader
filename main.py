a3a8162461c270d5d71d22346c27ef9324abfd57
Maxence-Santos <86566869+Maxence-Santos@users.noreply.github.com>
2021-08-29 18:25:35 +0200

    power_up added

--- main.py
+++ main.py
@@ -28,7 +28,16 @@ def main():
     pygame.display.set_caption("Space Invader")
     player = Player()
     ammos = []
+    fire_power_up = []
+    speed_power_up = []
+    time_f = 0
+    fire_t = 0
+    speed_t = 0
+    act_fire = False
+    act_speed = False
     fire_rate = 18 # The higher , the slower the player shoots
+    t_fire = []
+    t_speed = []
     
     for i in range(3):
             hearts.append(Lives(lives_X))
@@ -47,12 +56,69 @@ def main():
             if player.step > fire_rate:
                 ammos.append(Ammo(player.X))
                 player.step = 0 # Reset the step so the player can't shoot right away
+                
+        ######  CREATE POWER_UPS ######
+        if fire_t > 300:
+            fire_alea_X = random.randint(8,520)
+            if fire_alea_X in t_fire or fire_alea_X in t_speed:
+                fire_alea_X = random.randint(8,520)
+            t_fire.append(fire_alea_X)
+            t_fire.append(fire_alea_X-50)
+            t_fire.append(fire_alea_X+50)
+            fire_power_up.append(Speed_fire(fire_alea_X))
+            fire_t = 0
+        
+        if speed_t > 300:
+            speed_alea_X = random.randint(27,520)
+            if speed_alea_X in t_fire or speed_alea_X in t_speed:
+                speed_alea_X = random.randint(8,520)
+            t_speed.append(speed_alea_X)
+            t_speed.append(speed_alea_X-50)
+            t_speed.append(speed_alea_X+50)
+            speed_power_up.append(Speed(speed_alea_X))
+            speed_t = 0
+            
         ###### UPDATE SCREEN ########
-        player.update(input)     # Apply the changes depending on the input
+        if act_speed:
+            player.update_power(input)
+        else:
+            player.update(input)     # Apply the changes depending on the input
         SCREEN.fill(BACKGROUND)  # We place it here to overwrite the old dino image
         player.draw_image(SCREEN)
+        fire_t += 1
+        speed_t += 1
         for ammo in ammos:
             ammo.update_and_draw(SCREEN)
+        
+        for power in fire_power_up:
+            #act_fire = False
+            time_f = 0
+            fire_rate = 36
+            if power.X-50 <= player.X <= power.X+50:
+                fire_power_up.remove(power)
+                t_fire.remove(power.X)
+                t_fire.remove(power.X+50)
+                t_fire.remove(power.X-50)
+                act_fire = True
+                if act_fire:
+                    if time_f <= 300:
+                        time_f += 1
+                        fire_rate = 9
+                    else:
+                        act_fire = False
+            
+            power.update_and_draw(SCREEN)
+
+        for speed in speed_power_up:
+            act_speed = False
+            if speed.X-50 <= player.X <= speed.X+50:
+                speed_power_up.remove(speed)
+                t_speed.remove(speed.X)
+                t_speed.remove(speed.X+50)
+                t_speed.remove(speed.X-50)
+                act_speed = True
+            speed.update_and_draw(SCREEN)
+            
         pygame.display.update()  # Update the game with a 60 FPS rate
         CLOCK.tick(60)
 
